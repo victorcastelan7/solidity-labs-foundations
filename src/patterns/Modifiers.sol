691677811ba555f906b68310a67f997d83c0bb7c
Victor Castelan <victorcash7@gmail.com>
2025-12-08 00:01:17 -0800

    Add Modifiers contract and tests

--- src/patterns/Modifiers.sol
+++ src/patterns/Modifiers.sol
@@ -1,3 +1,171 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.30;
+
+contract Modifiers {
+  error Modifiers__NotOwner();
+  error Modifiers__InvalidAmount();
+  error Modifiers__Reentrancy();
+
+  address immutable private i_owner;
+  uint256 private s_value;
+  bool private s_locked;
+
+  constructor(uint256 initialValue) {
+    i_owner = msg.sender;
+    s_value = initialValue;
+  }
+
+  ///// MODIFIERS /////
+
+  modifier onlyOwner() {
+    if(msg.sender != i_owner) revert Modifiers__NotOwner();
+    _;
+  }
+
+  modifier noReentrancy() {
+    if(s_locked) revert Modifiers__Reentrancy();
+    s_locked = true;
+    _;
+    s_locked = false;
+  }
+
+  modifier nonZero(uint256 amount) {
+    if (amount == 0) revert Modifiers__InvalidAmount();
+    _;
+  }
+
+  ///// FUNCTIONS /////
+
+  function setValue(uint256 newValue) external onlyOwner nonZero(newValue) {
+    s_value = newValue;
+  }
+
+  function increment(uint256 amount) external nonZero(amount) {
+    s_value += amount;
+  }
+
+  function decrement(uint256 amount) external onlyOwner nonZero(amount) noReentrancy {
+    s_value -= amount;
+  }
+
+  function reset() external onlyOwner {
+    s_value = 0;
+  }
+
+  ///// GETTERS /////
+
+  function getValue() external view returns (uint256) {
+    return s_value;
+  }
+
+  function getOwner() external view returns (address) {
+    return i_owner;
+  }
+
+  function isLocked() external view returns (bool) {
+    return s_locked;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// contract ModifiersExample {
+//     // ----- Errors -----
+//     error Modifiers__NotOwner();
+//     error Modifiers__InvalidAmount();
+//     error Modifiers__Reentrancy();
+
+//     // ----- State -----
+//     address private immutable i_owner;
+//     bool private s_locked;
+//     uint256 private s_value;
+
+//     // ----- Constructor -----
+//     constructor(uint256 initialValue) {
+//         i_owner = msg.sender;
+//         s_value = initialValue;
+//     }
+
+//     // ----- Modifiers -----
+
+//     // Only the deployer / owner can call
+//     modifier onlyOwner() {
+//         if (msg.sender != i_owner) revert Modifiers__NotOwner();
+//         _;
+//     }
+
+//     // Make sure an amount is > 0
+//     modifier nonZero(uint256 amount) {
+//         if (amount == 0) revert Modifiers__InvalidAmount();
+//         _;
+//     }
+
+//     // Basic reentrancy guard
+//     modifier noReentrancy() {
+//         if (s_locked) revert Modifiers__Reentrancy();
+//         s_locked = true;
+//         _;
+//         s_locked = false;
+//     }
+
+//     // ----- Functions -----
+
+//     // Only owner, non-zero, sets the stored value
+//     function setValue(uint256 newValue)
+//         external
+//         onlyOwner
+//         nonZero(newValue)
+//     {
+//         s_value = newValue;
+//     }
+
+//     // Only owner, non-zero, protected by noReentrancy
+//     function decrement(uint256 amount)
+//         external
+//         onlyOwner
+//         nonZero(amount)
+//         noReentrancy
+//     {
+//         s_value -= amount;
+//     }
+
+//     function getValue() external view returns (uint256) {
+//         return s_value;
+//     }
+
+//     function getOwner() external view returns (address) {
+//         return i_owner;
+//     }
+
+//     function isLocked() external view returns (bool) {
+//         return s_locked;
+//     }
+// }
--- test/patterns/Modifiers.t.sol
+++ test/patterns/Modifiers.t.sol
@@ -1,3 +1,72 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.30;
+
+import { Test } from "forge-std/Test.sol";
+import { Modifiers } from "../../src/patterns/Modifiers.sol";
+
+contract TestModifiers is Test {
+  uint256 startingValue = 100;
+  uint256 amount = 1;
+  address owner;
+  address USER = makeAddr("user");
+  Modifiers modifiers;
+
+  function setUp() public {
+     modifiers = new Modifiers(startingValue);
+     owner = modifiers.getOwner();
+  }
+
+  function testStartingValueIncrements() public {
+    modifiers.increment(amount);
+
+    uint256 valueAfterIncrement = modifiers.getValue();
+
+    assertEq(startingValue + amount, valueAfterIncrement, "startingValue (100) incremented by amount (1), totaling 101");
+  }
+
+  function testRevertIfIncrementZero() public {
+    vm.expectRevert(Modifiers.Modifiers__InvalidAmount.selector);
+    modifiers.increment(0);
+  }
+
+  function testStartingValueDecrements() public {
+    vm.prank(owner);
+    modifiers.decrement(amount);
+
+    uint256 valueAfterDecrement = modifiers.getValue();
+
+    assertEq(startingValue - amount, valueAfterDecrement, "startingValue (100) decremented by amount (1), totaling 99");
+  }
+
+  function testRevertIfDecrementZero() public {
+    vm.prank(owner);
+    vm.expectRevert(Modifiers.Modifiers__InvalidAmount.selector);
+    modifiers.decrement(0);
+  }
+
+  function testRevertIfNotOwner() public {
+    vm.prank(USER);
+    vm.expectRevert(Modifiers.Modifiers__NotOwner.selector);
+    modifiers.decrement(amount);
+  }
+
+  function testValueResets() public {
+    modifiers.increment(amount);
+    vm.prank(owner);
+    modifiers.reset();
+
+    uint256 valueAfterReset = modifiers.getValue();
+
+    assertEq(valueAfterReset, 0, "Value is reset to 0");
+  }
+  
+  function testNewValueUpdatesOldValue() public {
+    uint256 newValue = 50;
+    vm.prank(owner);
+    modifiers.setValue(newValue);
+    uint256 valueAfterSetNewValue = modifiers.getValue();
+
+    assertEq(newValue, valueAfterSetNewValue, "Value set to 50");
+  }
+}
